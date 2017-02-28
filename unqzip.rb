3474c492c54428a76ea78b6ccac185e7e97c44d7
Yilin Chen <sticnarf@gmail.com>
2017-02-28 22:06:05 +0800

    Fix on windows

--- unqzip.rb
+++ unqzip.rb
@@ -1,7 +1,11 @@
 require 'zlib'
 require 'fileutils'
-filename = ARGV[0]
-data = File.read filename
+input_file = File::open ARGV[0], 'r'
+data = ""
+until input_file.eof?
+    data << input_file.read(256)
+end
+input_file.close
 offset = 2
 ec = Encoding::Converter.new('UTF-16BE', Encoding::default_external)
 
@@ -12,8 +16,6 @@ offset += len
 
 file_count = data.byteslice(offset...offset+4).unpack("N")[0]
 offset += 4
-
-puts file_count
 
 filenames = (1..file_count).map do
     len = data.byteslice(offset...offset+4).unpack("N")[0]
